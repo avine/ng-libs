5edc6c10915ea5a29d32cd795042629577ba0c2b
Stéphane Francel <stephane.francel@gmail.com>
2021-10-31 01:56:51 +0200

    Fix app test (add missing `moduleNameMapper`)

---
+++ jest.config.js
@@ -0,0 +1,13 @@
+module.exports = {
+  preset: 'jest-preset-angular',
+  setupFilesAfterEnv: ['<rootDir>/setup-jest.ts'],
+
+  /*
+   * To generate the `moduleNameMapper` value, execute the following code in Node:
+   * const mapper = require('jest-module-name-mapper');
+   * mapper.default();
+   */
+  moduleNameMapper: {
+    '^@avine/ng-if-non-nullish': '<rootDir>//projects/if-non-nullish/src/public-api.ts',
+  },
+};

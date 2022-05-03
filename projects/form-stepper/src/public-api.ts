4f577902aa8a69a540bf90c2d9a1e1031214c4f0
Stéphane Francel <stephane.francel@gmail.com>
2022-05-03 23:37:25 +0200

    Fix form-stepper build

--- projects/form-stepper/src/public-api.ts
+++ projects/form-stepper/src/public-api.ts
@@ -12,6 +12,7 @@ export * from './lib/form-stepper-icon/form-stepper-icon.component';
 export * from './lib/form-stepper-main/form-stepper-main.component';
 export * from './lib/form-stepper-nav/form-stepper-nav.component';
 export * from './lib/form-stepper-quicknav/form-stepper-quicknav.component';
+export * from './lib/form-stepper-section-icon/form-stepper-section-icon.component';
 
 export * from './lib/form-stepper-container/form-stepper-container.directive';
 export * from './lib/form-stepper-control/form-stepper-control.directive';

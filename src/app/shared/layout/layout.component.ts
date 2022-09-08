5f2969a94b7f88ade4de573bf7c47b899a9a293d
Stéphane Francel <stephane.francel@gmail.com>
2022-09-08 15:53:54 +0200

    App: responsive layout

--- src/app/shared/layout/layout.component.ts
+++ src/app/shared/layout/layout.component.ts
@@ -1,17 +1,18 @@
 import { Subscription } from 'rxjs';
 import { filter } from 'rxjs/operators';
 
-import { CommonModule } from '@angular/common';
-import { ChangeDetectionStrategy, Component, OnDestroy, OnInit, ViewChild } from '@angular/core';
+import { BreakpointObserver, Breakpoints, LayoutModule } from '@angular/cdk/layout';
+import { CommonModule, DOCUMENT } from '@angular/common';
+import { ChangeDetectionStrategy, Component, Inject, OnDestroy, OnInit, ViewChild } from '@angular/core';
 import { MatButtonModule } from '@angular/material/button';
 import { MatIconModule } from '@angular/material/icon';
-import { MatDrawer, MatSidenavModule } from '@angular/material/sidenav';
+import { MatDrawerMode, MatSidenav, MatSidenavContent, MatSidenavModule } from '@angular/material/sidenav';
 import { MatToolbarModule } from '@angular/material/toolbar';
 import { NavigationEnd, Router, RouterModule } from '@angular/router';
 
 import { MenuComponent } from './menu/menu.component';
 
-const materialModules = [MatButtonModule, MatIconModule, MatSidenavModule, MatToolbarModule] as const;
+const materialModules = [MatButtonModule, MatIconModule, MatSidenavModule, MatToolbarModule, LayoutModule] as const;
 
 @Component({
   standalone: true,
@@ -22,17 +23,47 @@ const materialModules = [MatButtonModule, MatIconModule, MatSidenavModule, MatTo
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class LayoutComponent implements OnInit, OnDestroy {
-  @ViewChild('sidenav') sidenav!: MatDrawer;
+  @ViewChild(MatSidenav) sidenav!: MatSidenav;
+
+  @ViewChild(MatSidenavContent) content!: MatSidenavContent;
+
+  drawerMode!: MatDrawerMode;
+
+  closeOnNavigationEnd!: boolean;
 
   private readonly subscription = new Subscription();
 
-  constructor(private router: Router) {}
+  constructor(
+    private router: Router,
+    @Inject(DOCUMENT) private document: Document,
+    private breakpointObserver: BreakpointObserver
+  ) {}
 
   ngOnInit() {
     this.subscription.add(
       this.router.events
         .pipe(filter((event): event is NavigationEnd => event instanceof NavigationEnd))
-        .subscribe(() => this.sidenav.close())
+        .subscribe(() => {
+          if (this.closeOnNavigationEnd) {
+            this.sidenav.close();
+          }
+
+          this.document.defaultView?.requestAnimationFrame(() =>
+            this.content.getElementRef().nativeElement.scrollTo(0, 0)
+          );
+        })
+    );
+
+    this.subscription.add(
+      this.breakpointObserver.observe(Breakpoints.Handset).subscribe(({ matches }) => {
+        if (matches) {
+          this.drawerMode = 'over';
+          this.closeOnNavigationEnd = true;
+        } else {
+          this.drawerMode = 'side';
+          this.closeOnNavigationEnd = false;
+        }
+      })
     );
   }
 
